b30a8ed9c03fab40aae0f09aeb677aac2e94ee94
Khoa <khoa.h.nguyen@tuni.fi>
2022-07-18 18:24:23 +0300

    Updates

---
+++ precache-manifest.95275731a4d63b663dba40e80f1436d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e451727c5690ec5a8f8c",
+    "url": "/quizzical/static/css/main.79ac068f.chunk.css"
+  },
+  {
+    "revision": "e451727c5690ec5a8f8c",
+    "url": "/quizzical/static/js/main.e451727c.chunk.js"
+  },
+  {
+    "revision": "724a4364c9ca27a909ae",
+    "url": "/quizzical/static/js/1.724a4364.chunk.js"
+  },
+  {
+    "revision": "10828be3562dbc652641",
+    "url": "/quizzical/static/js/runtime~main.10828be3.js"
+  },
+  {
+    "revision": "70fec2d767c7ae60be4a3edb50981c0f",
+    "url": "/quizzical/index.html"
+  }
+];
